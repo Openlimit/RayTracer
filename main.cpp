901c87bcc15264be16ce8c05630da9f189cc2b17
wanglingye <wanglingye@meidaitech.com>
2018-10-15 15:19:46 +0800

    update

--- main.cpp
+++ main.cpp
@@ -33,9 +33,9 @@ int main() {
     int nx = 200, ny = 100, ns = 100;
     outfile << "P3\n" << nx << " " << ny << "\n255\n";
     hitable *list[4];
-    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
     list[1] = new sphere(vec3(0, -100.5f, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
+    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.0));
     list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
     camera cam;
     hitable *world = new hitable_list(list, 4);
